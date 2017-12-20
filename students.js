4e94857a4ea0c940ad92c5578d8bb9e8cd07e0e2
cdmachado <cdmachado23@gmail.com>
2017-12-21 02:30:55 +0300

    almost done

--- students.js
+++ students.js
@@ -3,6 +3,10 @@ const ulList = document.querySelector('ul');
 const studentList2 = ulList.querySelectorAll('li');
 const studentList = Array.from(studentList2);
 let numPage;
+const parent1 = document.querySelector('div.pagination');
+let matchedList = [];
+
+
 
 
 function hiddeItems() {
@@ -23,14 +27,14 @@ function searchElements() {
   parentDiv.appendChild(searchInp);
 }
 
-function showPage(numPage, studentList) {
+function showPage(numPage, list2) {
   hiddeItems();
   let numPage10 = 10*numPage;
-  for ( i = 0; i < studentList.length; i++) {
-    let index = studentList[i];
-    let indexStu = studentList.indexOf(index);
+  for ( i = 0; i < list2.length; i++) {
+    let index = list2[i];
+    let indexStu = list2.indexOf(index);
     if (indexStu >= numPage10-10 && indexStu < numPage10) {
-        studentList[i].style.display = 'block';
+        list2[i].style.display = 'block';
     }
   }
 }
@@ -45,9 +49,8 @@ function kickStart() {
   appendPageLinks(studentList);
 }
 
-function appendPageLinks(studentList) {
-  let nPages = Math.ceil(studentList.length/10);
-  const parent1 = document.querySelector('div.pagination');
+function appendPageLinks(stuList) {
+  let nPages = Math.ceil(stuList.length/10);
   const ulCreated = document.createElement('ul');
   parent1.appendChild(ulCreated);
 
@@ -68,10 +71,36 @@ function appendPageLinks(studentList) {
 }
 
 function searchList() {
+  matchedList = [];
+  let input = document.querySelector('input');
   let userSearch = input.value;
+  let numUl = document.getElementsByTagName('ul');
+  if (numUl.length > 1) {
+    const remov = parent1.querySelector('ul');
+    parent1.removeChild(remov);
+  }
+  for (i =0; i < studentList.length; i++) {
+    let stuLi = studentList[i];
+    let stuDiv = stuLi.querySelector('div');
+    let stuH3 = stuDiv.querySelector('h3');
+    let stuSpan = stuDiv.querySelector('span');
+    let name = stuH3.innerHTML;
+    let email = stuSpan.innerHTML;
+    if (name.includes(userSearch) == true || email.includes(userSearch) == true) {
+      matchedList.push(studentList[i]);
+    }
+  }
+  if ( matchedList == '') {
+      alert("No student's found");
+  } else if ( matchedList.length > 10 ) {
+
+    appendPageLinks(matchedList);
+  }
+
+  showPage(1, matchedList);
 }
 
-debugger;
+
 kickStart();
 
 $('.pagination a').on('click', (event) => {
@@ -79,4 +108,9 @@ $('.pagination a').on('click', (event) => {
   event.target.className = 'active';
   numPage = event.target.innerHTML;
   showPage(numPage, studentList)
+});
+
+$('button').on('click', () => {
+    debugger;
+    searchList();
 });
